bda8b2e56f7b92ef4e434ad4e3cdaf555f933726
PravinPadalkar <pravin.padalkar@vit.edu.in>
2023-04-26 19:57:12 +0530

    Added

--- src/components/Profile/profileitems.js
+++ src/components/Profile/profileitems.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import { useRef } from "react";
 import logo1 from '../imges/profile1.jpg'
 import './profile.css'
 import { Link } from 'react-router-dom'
 const profileitems = (props) => {
-	const modalEle = useRef(null);
-	const refClose = useRef(null);
   
-	const update = () => {
-	  modalEle.current.click();
-	};
-  
-	const handleOnClick = () => {
-	  refClose.current.click();
-	};
 	return (
 		<div className="col-md-3" style={{margin :"0px 1px "}}>
 			<div className="card profile" style={{ width: "20rem"  }}>
@@ -25,7 +15,7 @@ const profileitems = (props) => {
 					<p className="card-text"><b>Address:</b> {props.address}</p>
 					<p><b>Phone Number:</b>{props.mobile} </p>
 					<p><b>Telephone Number:</b>{props.tmob}</p>
-					<Link to="/modal" ref={modalEle} onClick={update} className="btn btn-success">Show More</Link>
+					<Link to="/modal"onClick={update} className="btn btn-success">Show More</Link>
 				</div>
 			</div>
 		</div>
